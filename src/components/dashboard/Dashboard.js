af0e9f468a71233f215d45f0d513454d1027c55e
JuliansRecent <juliansrecent@gmail.com>
2022-12-25 19:15:47 -0800

    Add calendar to the dashboard.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import Navbar from '../navbar/Navbar';
+import {useState} from 'react'
+import Calendar from 'react-calendar'
+
 import Header from '../header/Header'
 import Footer from '../footer/Footer'
+import '../dashboard/Dashboard.css'
 
 function Dashboard(props) {
+    const [date, setDate] = useState(new Date())
+
+
     return (
-        <div>
-            <Header/>
-            <Footer/>
-        </div>
-    );
+			<div>
+				<Header />
+				<div className='calendar-container d-flex justify-content-center mt-5 mb-3'>
+					<Calendar onChange={setDate} value={date}/>
+				</div>
+                <div className='text-center'>Selected date: {date.toDateString()}</div>
+				<Footer />
+			</div>
+		);
 }
 
 export default Dashboard;
